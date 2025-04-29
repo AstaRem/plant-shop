f404a72a0febb9e40f59994d5d079919caf87c5d
Asta Remeikiene <asta.rem@hotmail.com>
2025-04-29 09:57:53 +0300

    ferntastic deals done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header"
 import Hero from "./Components/Hero"
 import SectionBest from "./Components/SectionBest";
 import SectionCategories from "./Components/SectionCategories";
+import SectionDeals from "./Components/SectionDeals";
 import SectionHot from "./Components/SectionHot";
 import TopBanner from "./Components/TopBanner"
 
@@ -17,6 +18,7 @@ function App() {
       <SectionCategories />
       <SectionBest/>
       <SectionHot />
+      <SectionDeals />
     </>
   );
 }
---
+++ src/Components/Image.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import emptySvg from '../assets/empty.svg';
+
+export default function Image({src, alt, className}) {
+
+    const [imgSrc, setImageSrc] =  useState(src);
+
+    return(
+        <img 
+            src={imgSrc} 
+            alt={alt} 
+            className={className} 
+            onError={() => setImageSrc(emptySvg)} 
+        />
+    )
+}
